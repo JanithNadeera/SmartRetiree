ddb100968ea923bd57b292fff6020608e3474bb8
Thushan Karunathilaka <dthushan829@gmail.com>
2025-04-17 18:35:54 +0530

    chore: Comment out unused firebase_utils import in home_screen.dart

--- lib/ui/mp/home/home_screen.dart
+++ lib/ui/mp/home/home_screen.dart
@@ -3,7 +3,7 @@ import 'package:flutter/material.dart';
 import 'package:smart_retiree/models/app_post.dart';
 import 'package:smart_retiree/ui/mp/home/post_view.dart';
 import 'package:smart_retiree/ui/mp/home/widgets/home_app_bar.dart';
-import 'package:smart_retiree/utils/firebase_utils.dart';
+// import 'package:smart_retiree/utils/firebase_utils.dart';
 import 'package:smart_retiree/utils/loader.dart';
 import 'package:smart_retiree/utils/theme_extension.dart';
 
